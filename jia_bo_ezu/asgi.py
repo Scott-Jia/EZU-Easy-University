c3142ec5056eef43626417e4c3673032beadedd1
Scott-Jia <bojia2020@gmail.com>
2021-02-21 14:17:53 -0600

    set up, created courseinfo app and wrote model class

---
+++ jia_bo_ezu/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jia_bo_ezu project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jia_bo_ezu.settings')
+
+application = get_asgi_application()
