e817a994c3c7231e6d840ba4e58171a006cff6e3
Scott-Jia <bojia2020@gmail.com>
2021-02-27 17:31:58 -0600

    create templates and update views.py and urls.py

--- jia_bo_ezu/urls.py
+++ jia_bo_ezu/urls.py
@@ -9,13 +9,28 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URConf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 
+from courseinfo.views import (
+    instructor_list_view,
+    section_list_view,
+    course_list_view,
+    semester_list_view,
+    student_list_view,
+    registration_list_view,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('instructor/', instructor_list_view),
+    path('section/', section_list_view),
+    path('course/', course_list_view),
+    path('semester/', semester_list_view),
+    path('student/', student_list_view),
+    path('registration/', registration_list_view),
 ]
