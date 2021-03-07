3b9c31b7de6442d243b6836ee961fe55978e4714
Scott-Jia <bojia2020@gmail.com>
2021-03-07 02:02:06 -0600

    complete the refactor of urls.py

--- courseinfo/urls.py
+++ courseinfo/urls.py
@@ -13,10 +13,22 @@ Including another URConf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from courseinfo.views import (
+    instructor_list_view,
+    section_list_view,
+    course_list_view,
+    semester_list_view,
+    student_list_view,
+    registration_list_view,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('courseinfo.urls')),
+    path('instructor/', instructor_list_view),
+    path('section/', section_list_view),
+    path('course/', course_list_view),
+    path('semester/', semester_list_view),
+    path('student/', student_list_view),
+    path('registration/', registration_list_view),
 ]
