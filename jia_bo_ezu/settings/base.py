c2eddd90e6f0818f5926226a99bf3e1b59f6cc56
Scott-Jia <bojia2020@gmail.com>
2021-04-25 22:08:08 -0500

    create multiple settings files

--- jia_bo_ezu/settings/base.py
+++ jia_bo_ezu/settings/base.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'jia_bo_ezu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': str(BASE_DIR / '../db.sqlite3'),
     }
 }
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 
